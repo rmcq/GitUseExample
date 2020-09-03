fab9c18a22ca8f18d062fb7010c16506591ac03f
Rob McQualter <rmcqualter@ausoptom.com>
2020-09-03 15:31:37 +1000

    Added map feature.

---
+++ GitUseExample/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseExample
+//
+//  Created by Rob on 3/9/20.
+//  Copyright © 2020 Optical Directions Consulting. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
